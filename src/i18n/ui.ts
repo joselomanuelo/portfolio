2cf87f782ae31d00b3cbd1b656784826e726317f
joselomanuelo <joselomanuelo@gmail.com>
2025-07-17 11:59:51 -0500

    feat: implement i18n structure

---
+++ src/i18n/ui.ts
@@ -0,0 +1,13 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {},
+  es: {},
+} as const;
